ef5484a4a33ede2a6994cb92604616f43e19f2a0
HzamaDai <danihizawa@gmail.com>
2022-04-05 05:21:21 +0700

    meet4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/ui/produk_form.dart';
+import 'package:flutter_application_1/ui/produk_detail.dart';
+import 'package:flutter_application_1/ui/produk_page.dart';
+
+import 'package:flutter_application_1/ui/produk_form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,156 +11,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    Color color = Theme.of(context).primaryColor;
-
-    Widget buttonSection = Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        _buildButtonColumn(color, Icons.call, 'CALL'),
-        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-        _buildButtonColumn(color, Icons.share, 'SHARE'),
-      ],
-    );
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: [
-            Image.network(
-              'https://raw.githubusercontent.com/flutter/website/master/examples/layout/lakes/step5/images/lake.jpg',
-              width: 300,
-              height: 240,
-              fit: BoxFit.cover,
-            ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ],
-        ),
-      ),
+      title: "KATALOG PRODUK",
+      home: ProdukForm(),
     );
-  }
-
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /*1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /*2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: const Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-      /*3*/
-      Icon(
-        Icons.star,
-        color: Colors.red[500],
-      ),
-      const Text('41'),
-    ],
-  ),
-);
-
-Widget textSection = const Padding(
-  padding: EdgeInsets.all(32),
-  child: Text(
-    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-    'Alps. Situated 1,578 meters above sea level, it is one of the '
-    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-    'half-hour walk through pastures and pine forest, leads you to the '
-    'lake, which warms to 20 degrees Celsius in the summer. Activities '
-    'enjoyed here include rowing, and riding the summer toboggan run.',
-    softWrap: true,
-  ),
-);
-
-class ProdukForm extends StatefulWidget {
-  const ProdukForm({Key? key}) : super(key: key);
-
-  @override
-  _ProdukFormState createState() => _ProdukFormState();
-}
-
-class _ProdukFormState extends State<ProdukForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Form Produk'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            _textboxKodeProduk(),
-            _textboxNamaProduk(),
-            _textboxHargaProduk(),
-            _tombolSimpan()
-          ],
-        ),
-      ),
-    );
-  }
-
-  _textboxKodeProduk() {
-    return TextField(
-        decoration: const InputDecoration(labelText: "Kode Produk"));
-  }
-
-  _textboxNamaProduk() {
-    return TextField(
-        decoration: const InputDecoration(labelText: "Nama Produk"));
-  }
-
-  _textboxHargaProduk() {
-    return TextField(decoration: const InputDecoration(labelText: "Harga"));
-  }
-
-  _tombolSimpan() {
-    return ElevatedButton(onPressed: () {}, child: const Text('Simpan'));
   }
 }
